8ba9dfd35bdce1b0c568346b705e2e7e35cb08e5
Nir Gluzman <nir.gluzman@gmail.com>
2025-07-15 00:32:23 +0200

    feat: add Lambda function for GET `/restaurants` endpoint

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -36,3 +36,45 @@ module "get_index_lambda" {
   # CloudWatch Logs retention to control costs and storage
   cloudwatch_logs_retention_in_days = 7  # Keep logs for 1 week
 }
+
+
+module "get_restaurants_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 7.0"
+
+  function_name = "${var.service_name}-get-restaurants"
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+
+  source_path = [{
+    path = "${path.module}/../functions/get-restaurants"
+  }]
+
+  environment_variables = {
+    default_results = "10"  # Default number of restaurants to return
+    restaurants_table = module.dynamodb_restaurants_table.dynamodb_table_id
+  }
+
+  attach_policy_statements = true
+  policy_statements = {
+    dynamodb_read = {
+      effect = "Allow"
+      actions = [
+        "dynamodb:Scan"
+      ]
+      resources = [module.dynamodb_restaurants_table.dynamodb_table_arn]
+    }
+  }
+
+  publish = true
+
+  allowed_triggers = {
+    APIGatewayGet = {
+      service    = "apigateway"
+      source_arn = "${aws_api_gateway_rest_api.main.execution_arn}/${var.stage_name}/GET/restaurants"
+    }
+  }
+
+  # CloudWatch Logs retention to control costs and storage
+  cloudwatch_logs_retention_in_days = 7  # Keep logs for 1 week
+}
