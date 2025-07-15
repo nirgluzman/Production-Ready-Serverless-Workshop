7449c6bd9c2f48787d25790b68fdfb832ded0c66
root <root@Laptop-Nir.>
2025-07-15 12:38:19 +0200

    feat: render restaurants in `index.html` using Mustache

--- functions/get-index/index.mjs
+++ functions/get-index/index.mjs
@@ -1,13 +1,34 @@
-import fs from 'fs';
-
-// The variable is declared and assigned OUTSIDE the handler function.
+// Declared and assigned OUTSIDE the handler function.
 // The declaration and assignment code will run ONLY the first time our code executes in a new worker instance.
 // This helps improve performance and allows us to load and cache static data only on the first invocation, which helps improve performance on subsequent invocations.
-const html = fs.readFileSync('static/index.html', 'utf-8');
+import fs from 'fs';
+import Mustache from 'mustache'; // templating engine to inject dynamic data into HTML
 
-// AWS Lambda handler function - serves static HTML page for GET requests
-// Returns cached HTML content with appropriate headers for browser rendering
+const restaurantsApiRoot = process.env.restaurants_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let html;
+
+function loadHtml() {
+  if (!html) {
+    console.log('loading index.html...');
+    html = fs.readFileSync('static/index.html', 'utf-8');
+    console.log('loaded');
+  }
+
+  return html;
+}
+
+const getRestaurants = async () => {
+  const resp = await fetch(restaurantsApiRoot); // load the list of restaurants from the GET /restaurants endpoint
+  return await resp.json();
+};
+
 export const handler = async (event, context) => {
+  const template = loadHtml();
+  const restaurants = await getRestaurants();
+  const dayOfWeek = days[new Date().getDay()];
+  const html = Mustache.render(template, { dayOfWeek, restaurants });
   const response = {
     statusCode: 200,
     headers: {
