e2e9bf4ecab597fe1397a10037803c546907a0e7
Nir Gluzman <nir.gluzman@gmail.com>
2025-07-16 15:26:59 +0200

    feat: integrate `aws4fetch` and `@aws-sdk/credential-providers` to enable authenticated requests to API Gateway's /restaurants endpoint

--- functions/get-index/index.mjs
+++ functions/get-index/index.mjs
@@ -3,9 +3,22 @@
 // This helps improve performance and allows us to load and cache static data only on the first invocation, which helps improve performance on subsequent invocations.
 import fs from 'fs';
 import Mustache from 'mustache'; // templating engine to inject dynamic data into HTML
+import { AwsClient } from 'aws4fetch'; // signing utility - AWS client for making authenticated requests to AWS services
+import { fromNodeProviderChain } from '@aws-sdk/credential-providers'; // AWS SDK utility to get credentials from the default provider chain
 
 const restaurantsApiRoot = process.env.restaurants_api;
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Create an AWS client to make authenticated requests to AWS services.
+// This client will be used to make requests to the AWS API Gateway to retrieve restaurant data.
+// https://dev.to/aws-builders/signing-requests-with-aws-sdk-in-lambda-functions-476
+const credentialProvider = fromNodeProviderChain(); // Set up the "chain" of locations the SDK will check for credentials.
+const credentials = await credentialProvider(); // Obtain the temporary credentials from the Lambda execution role.
+const aws = new AwsClient({
+  accessKeyId: credentials.accessKeyId,
+  secretAccessKey: credentials.secretAccessKey,
+  sessionToken: credentials.sessionToken,
+});
 
 let html;
 
@@ -19,8 +32,15 @@ function loadHtml() {
   return html;
 }
 
+// Fetch restaurant data from your API Gateway endpoint.
 const getRestaurants = async () => {
-  const resp = await fetch(restaurantsApiRoot); // load the list of restaurants from the GET /restaurants endpoint
+  const resp = await aws.fetch(restaurantsApiRoot); // Make an authenticated HTTP request using AWS credentials to load the list of restaurants from the GET /restaurants endpoint.
+
+  // Throw error if request failed (4xx, 5xx status codes).
+  if (!resp.ok) {
+    throw new Error('Failed to fetch restaurants: ' + resp.statusText);
+  }
+
   return await resp.json();
 };
 
