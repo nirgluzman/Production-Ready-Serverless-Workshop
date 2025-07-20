b182be8b2d1d7aa51d0182a2c35d94f19c91d5fd
Nir Gluzman <nir.gluzman@gmail.com>
2025-07-20 15:55:35 +0200

    feat: add `aws_caller_identity` data source for AWS account information

---
+++ terraform/data.tf
@@ -0,0 +1,13 @@
+/**
+ * AWS Data Sources
+ *
+ * This file defines data sources that retrieve information from AWS without creating any resources.
+ * These are used throughout the Terraform configuration to reference existing AWS resources or account information.
+ */
+
+# Retrieves information about the AWS account and IAM principal (user/role) that Terraform is using to perform operations.
+# This provides access to:
+# - account_id: The AWS account ID
+# - user_id: The unique identifier of the AWS principal
+# - arn: The Amazon Resource Name (ARN) of the principal
+data "aws_caller_identity" "current" {}
